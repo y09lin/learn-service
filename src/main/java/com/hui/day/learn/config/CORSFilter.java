0920f24aef3096c9eafbdc09c0eec14872f0cc9e
huim_lin <huim_lin@haihangyun.com>
2018-10-12 17:25:09 +0800

    添加跨域配置

---
+++ src/main/java/com/hui/day/learn/config/CORSFilter.java
@@ -0,0 +1,30 @@
+package com.hui.day.learn.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Angela on 17/7/19.
+ */
+@Component
+public class CORSFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain
+            filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With,token, " +
+                "Content-Type, Accept, Access-Token");
+        filterChain.doFilter(request, response);
+    }
+
+}
